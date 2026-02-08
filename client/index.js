9b1b44f7a2944410b720275301003abed5846abd
JAHNAVIJAGASANDRAM <jjahnavi2502@gmail.com>
2026-02-08 15:02:31 +0530

    proving server we are on the nice list

--- client/index.js
+++ client/index.js
@@ -6,9 +6,15 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
-
+  const name=niceList[0];
+  const merkleTree = new MerkleTree(niceList);
+  const proof = merkleTree.getProof(name);
+  console.log({ name });
+  console.log({ prooflength:proof.length });
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+     name,
+     proof,
+     // TODO: add request body parameters here!
   });
 
   console.log({ gift });
